d64cdd70d58abe378c600e7c0c078f5c4bd5acf8
LPB-Devin <252834399@qq.com>
2019-09-01 18:54:37 +0800

    First-commit

---
+++ src/tmall/util/DBUtil.java
@@ -0,0 +1,37 @@
+
+package tmall.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	static String ip = "127.0.0.1";
+	static int port = 3306;
+	static String database = "tmall";
+	static String encoding = "UTF-8";
+	static String loginName = "root";
+	static String password = "lin8808348";
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s&useUnicode=true&serverTimezone=GMT", ip, port, database, encoding);
+		return DriverManager.getConnection(url, loginName, password);
+	}
+	
+	public static void main(String[] args) throws SQLException {
+		System.out.println(getConnection());
+		
+	}
+
+	
+
+}
+
